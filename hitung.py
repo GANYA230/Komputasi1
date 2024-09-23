e4da6f78d7e90d452aa252c793eb9fa5384dad2f
GANYA230 <ganyacool230@gmail.com>
2024-09-23 10:04:44 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
-sx = st.text_input("satuan", "C")
-st.write("Anda memasukkan", x, ' ',sx)
+sx = st.text_input("satuan awal", "C")
+fx = st.text_input("satuan akhir", "R")
+y = 0
+
+if(sx=='C'):
+  if(fx=='C'):
+    print(x)
+  elif(fx=='R'):
+      print(x*4/5)
+
+konversi()
+st.write(x, ' ', sx, "=", y, fx)
 
