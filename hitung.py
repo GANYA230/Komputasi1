7cc8efa101254ad0003ba5c5691d246785a1e74f
GANYA230 <ganyacool230@gmail.com>
2024-09-23 10:20:04 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -14,6 +14,33 @@ if(sx=='C'):
     y = (x+32)*5/9
   elif(fx=='K'):
     y = x + 273
+elif(sx=='R'):
+  if(fx=='C'):
+    y = x*5/4
+  elif(fx=='R'):
+    x = y 
+  elif(fx=='F'):
+    y = (x+32)*4/9
+  elif(fx=='K'):
+    y = x*5/4+273
+elif(sx=='F'):
+  if(fx=='C'):
+    y = (x-32)*5/9
+  elif(fx=='R'):
+    x = (x-32)*4/9
+  elif(fx=='F'):
+    x = y
+  elif(fx=='K'):
+    y = ((x-32)*5/9)+273
+elif(sx=='K'):
+  if(fx=='C'):
+    y = x-273
+  elif(fx=='R'):
+    x = (x-273)*4/5
+  elif(fx=='F'):
+    x = (x-273)*9/5+32
+  elif(fx=='K'):
+    x = y
 
 st.write(x, ' ', sx, "=", y, fx)
 
