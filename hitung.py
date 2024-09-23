190f369464a4c392f7e66813577f8304f3cd101c
GANYA230 <ganyacool230@gmail.com>
2024-09-23 10:13:02 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -9,7 +9,11 @@ if(sx=='C'):
   if(fx=='C'):
     x = y
   elif(fx=='R'):
-      y = x*4/5 
+    y = x*4/5 
+  elif(fx=='F'):
+    y = (x+32)*5/9
+  elif(fx=='K'):
+    y = x + 273
 
 st.write(x, ' ', sx, "=", y, fx)
 
